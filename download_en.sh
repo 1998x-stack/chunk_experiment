959c88f80059f83be1fce63e8fbefdfa0d8d4b5f
Yuuki <z2109157583@163.com>
2024-12-27 18:03:51 +0800

    modify

--- download_en.sh
+++ download_en.sh
@@ -5,7 +5,7 @@ git clone git@github.com:computationalstylistics/100_english_novels.git
 
 # 定义源文件夹和目标文件夹
 source_folder="100_english_novels"
-destination_folder="/data/general/en/"
+destination_folder="data/general/en/"
 
 # 创建目标文件夹（如果不存在的话）
 mkdir -p "$destination_folder"
@@ -13,4 +13,7 @@ mkdir -p "$destination_folder"
 # 查找并移动所有txt文件
 find "$source_folder" -type f -name "*.txt" -exec mv {} "$destination_folder" \;
 
-echo "所有txt文件已成功移动到 $destination_folder"
+# 删除源文件夹
+rm -rf "$source_folder"
+
+echo "所有txt文件已成功移动到 $destination_folder，并删除了源文件夹 $source_folder"
